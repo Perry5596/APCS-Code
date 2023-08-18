475f597a73fe06cafb3081a625988d6cf067f62b
Perry5596 <jdbrennan07@gmail.com>
2023-08-17 20:34:50 -0500

    Added U1.L1.CA1 and CA2

---
+++ U1.L1.CA2/U1_L1_Activity_Two.java
@@ -0,0 +1,13 @@
+/* Lesson 1 Coding Activity Question 2 */
+
+class U1_L1_Activity_Two 
+{
+  public static void main(String[] args) 
+  {
+    
+    /* Write your code here */
+    System.out.println(" Coding is fun! ");
+    System.out.println(" Coding is fun! ");
+    System.out.println(" Coding is fun! ");
+  }
+}
